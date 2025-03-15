f44c0df700b8fadfac097371ede2ad914ddc6600
Your GitHub Username <angelangelis314@gmail.com>
2025-03-14 20:42:37 -0700

    added dark mode toggle

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -8,6 +8,14 @@ hamMenu.addEventListener("click", () => {
   offScreenMenu.classList.toggle("active");
 });
 
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode'); // Toggles dark mode
+    darkModeToggle.textContent = body.classList.contains('dark-mode') ? '☀ Light Mode' : '🌙 Dark Mode';
+});
+
 document.getElementById("year").textContent = new Date ().getFullYear();
 
 document.getElementById("last-modified").textContent = document.lastModified;
