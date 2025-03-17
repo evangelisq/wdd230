5872a826268bcf65efbacad4cb7afb32e861c8ca
Your GitHub Username <angelangelis314@gmail.com>
2025-03-17 00:19:30 -0700

    added new elements

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,12 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e){
+        e.preventDefault();
+        document.querySelector(this.getAttrbute('heref')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 document.getElementById("year").textContent = new Date ().getFullYear();
 
 document.getElementById("last-modified").textContent = document.lastModified;
