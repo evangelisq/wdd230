1c92605ac5a0f97a1e22d117e789ade589391e85
Your GitHub Username <angelangelis314@gmail.com>
2025-03-18 01:10:53 -0700

    some cganges

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,9 +1,17 @@
-const hamburger = document.querySelector('hamburger-menu');
+const hamburger = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.menu');
 
 hamburger.addEventListener ('click', () => {
-    menu.style.display = menu.style.display === 'flex'? 'none': 'flex'; 
-})
+  menu.style.display = menu.style.display === 'flex'? 'none': 'flex'; 
+}); 
+   
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+});
 
 document.getElementById("year").textContent = new Date ().getFullYear();
 
