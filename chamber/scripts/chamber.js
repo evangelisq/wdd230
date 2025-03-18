70f7b152567433a7124a81bdbfe630b1ab015acb
Your GitHub Username <angelangelis314@gmail.com>
2025-03-17 20:55:00 -0700

    added

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector('hamburger-menu');
 const menu = document.querySelector('.menu');
 
 hamburger.addEventListener ('click', () => {
-    menu.computedStyleMap.display = menu.style.display === 'flex'? 'none': 'flex'; 
+    menu.style.display = menu.style.display === 'flex'? 'none': 'flex'; 
 })
 
 document.getElementById("year").textContent = new Date ().getFullYear();
