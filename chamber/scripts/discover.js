f80b12c7360528b5bd671df0f15c4c6a61a7bb7a
Your GitHub Username <angelangelis314@gmail.com>
2025-03-23 00:40:20 -0700

    added

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,3 +1,23 @@
+
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode'); // Toggles dark mode
+    darkModeToggle.textContent = body.classList.contains('dark-mode') ? '☀ Light Mode' : '🌙 Dark Mode';
+});
+
+
+
 const now = Date.now();
 const lastVisit = localStorage.getItem('lastVisit');
 const visitMessage = document.getElementById('visit-message');
