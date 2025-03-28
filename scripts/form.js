93a1bb8e5356121e4993d29d5258315671467617
Your GitHub Username <angelangelis314@gmail.com>
2025-03-28 00:12:37 -0700

    changed

--- scripts/form.js
+++ scripts/form.js
@@ -1,4 +1,19 @@
+const hamMenu = document.querySelector(".ham-menu");
 
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode'); // Toggles dark mode
+    darkModeToggle.textContent = body.classList.contains('dark-mode') ? '☀ Light Mode' : '🌙 Dark Mode';
+});
 
 document.getElementById("year").textContent = new Date ().getFullYear();
 
