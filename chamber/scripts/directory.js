a6cc57bd3425caa61e37238490d9807bc0d381c7
Your GitHub Username <angelangelis314@gmail.com>
2025-04-07 18:56:29 -0700

    grid

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,14 +1,18 @@
 fetch('data/members.json')
 .then(response => response.json()) 
 .then(data => {
-    const container = document.getElementById('members-container');
+    const container = document.getElementById('members-containers');
      data.forEach(member => {
-        const memberCard = `<div class = "member-card">
+        console.log(member.image);
+        console.log('Toggle view to: ${viewType}');
+
+        const memberCard = 
+        `<div class = "member-card">
             <img src="${member.image}" alt="${member.name}">
             <h2>${member.name}</h2>
             <p>${member.address}</p>
             <p>${member.phone}</p>
-            <p><a heref="${member.website}" target="_blank">Visit Website</a></p>
+            <p><a href="${member.website}" target="_blank">Visit Website</a></p>
             <p>Membership Level: ${member.membership_level}</p>
         </div>`;
         container.innerHTML += memberCard;
@@ -16,8 +20,8 @@ fetch('data/members.json')
 })
 .catch(error => console.error('Error loading data:', error));
 
-function toogleView(viewType) {
-    const contaier = document.getElementById('members-container');
+function toggleView(viewType) {
+    const container = document.getElementById('members-containers');
     if (viewType  == 'grid'){
         container.classList.remove('list-view');
         contaier.classList.add('grid-view');
